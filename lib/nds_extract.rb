4971c63382d0e3b9176e6e9450dc9a11a3ea65f2
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-26 19:46:19 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -16,6 +16,10 @@ def directors_totals(nds)
   while row_index < nds.length do
     column_index = 0
 
+    while column_index < nds[row_index]
+
+    end
+
     while column_index < nds[row_index][:movies].length do
       total += nds[row_index][:movies][column_index][:worldwide_gross]
       nds[row_index][:movies]
